35fec42c8188580f593146e6a46ce06456b2946b
valntyn <romanenko.valentyn1@gmail.com>
2023-02-28 19:12:52 +0200

    created store

---
+++ src/Components/Phones/Phones.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Product } from '../../types/Product';
+import { SkeletonLoading } from '../SkeletonLoading';
+import { Card } from '../Card';
+
+type PropTypes = {
+  products: Product[]
+  isLoading?: boolean;
+};
+
+export const Phones: React.FC<PropTypes> = ({ products, isLoading }) => {
+  return (
+    <>
+      {isLoading ? (<SkeletonLoading />)
+        : (
+          products.map((product) => (
+            <Card
+              product={product}
+              key={product.id}
+            />
+          ))
+        )}
+    </>
+  );
+};
